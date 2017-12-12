eb99a4cb3ca79dedf75a52405b3c38181ec4f4a2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-12 15:28:50 +0000

    Tue Dec 12 15:28:50 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -7,18 +7,25 @@ public class FizzBuzz {
     public static String fizzBuzz(Integer number) {
         
 //        String r = number.toString();
-        String r = String.format("%s%s", (number % 3 == 0) ? "fizz" : "",
-                                         (number % 5 == 0) ? " buzz" : "");
+//        String r = String.format("%s%s", (number % 3 == 0) ? "fizz" : "",
+//                                         (number % 5 == 0) ? " buzz" : "");
+//        
         
         
         
         
-        
-        if (number % 3 == 0) r = "fizz";
-        if (number % 5 == 0) r += "buzz";
-        
-        
-        
+             if (multipleOf3(number)) return "fizz";
+        else if (multipleOf5(number)) return "buzz";
+        else if (multipleOf3(number) && multipleOf5(number)) return "fizz buzz";
+        else return number.toString();
+    }
+
+    private static boolean multipleOf5(Integer number) {
+        return number % 5 == 0;
+    }
+
+    private static boolean multipleOf3(Integer number) {
+        return number % 3 == 0;
     }
 
 }
