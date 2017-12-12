0fe1421303e59e80c00190e03967e9696c5d6213
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-12 15:25:50 +0000

    Tue Dec 12 15:25:50 GMT 2017

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -6,9 +6,15 @@ public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
         
-        String r = "";
+//        String r = number.toString();
+        String r = String.format("%s%s", (number % 3 == 0) ? "fizz" : "",
+                                         (number % 5 == 0) ? " buzz" : "");
         
-        if (number % 3 == 0) r += "fizz";
+        
+        
+        
+        
+        if (number % 3 == 0) r = "fizz";
         if (number % 5 == 0) r += "buzz";
         
         
